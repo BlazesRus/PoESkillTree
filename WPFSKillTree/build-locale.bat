40702f29f651323c665fe96bb5e869d0c0fcc0e3
BlazesRus <BlazesRus@yahoo.com>
2017-11-30 22:32:08 -0500

    Enable VS2017 installation detecting if have vswhere.exe from microsoft inside folder

--- WPFSKillTree/build-locale.bat
+++ WPFSKillTree/build-locale.bat
@@ -4,7 +4,21 @@
 
 PUSHD %~dp0
 
+if NOT exist "vswhere.exe" GOTO DEFAULTBUILDLOCALE
+
+REM VSWhere from https://github.com/Microsoft/vswhere use to locate highest installed VS Installation
+for /f "usebackq tokens=1* delims=: " %%i in (`vswhere.exe -latest -requires Microsoft.VisualStudio.Workload.NativeDesktop`) do (
+if /i "%%i"=="installationPath" set VSDir=%%j
+)
+ECHO "Visual Studio 2017 Detected as installed at "%VSDir%
+IF "%VS2017TOOLS%" == "" GOTO DEFAULTBUILDLOCALE
+
+IF [%1] == [] cmd /C "%VSDir%\Common7\Tools\vsdevcmd.bat && cd ""%~dp0"" && msbuild release.xml /target:BuildLocale"
+IF NOT [%1] == [] cmd /C "%VSDir%\Common7\Tools\vsdevcmd.bat && cd ""%~dp0"" && msbuild release.xml /target:BuildAndCopyLocale /property:LocaleTargetDir=%1"
+
+:DEFAULTBUILDLOCALE
 IF [%1] == [] cmd /C "vsdevcmd.bat && cd ""%~dp0"" && msbuild release.xml /target:BuildLocale"
 IF NOT [%1] == [] cmd /C "vsdevcmd.bat && cd ""%~dp0"" && msbuild release.xml /target:BuildAndCopyLocale /property:LocaleTargetDir=%1"
+:END
 
 POPD
