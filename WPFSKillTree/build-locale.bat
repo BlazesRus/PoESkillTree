528a1a65e832d2163ae89a4f1ca8fe23594a5520
BlazesRus <BlazesRus@yahoo.com>
2017-12-04 22:09:18 -0500

    Fixed Missing GOTO instruction in build-locale

--- WPFSKillTree/build-locale.bat
+++ WPFSKillTree/build-locale.bat
@@ -4,7 +4,9 @@
 
 PUSHD %~dp0
 
-if NOT exist "vswhere.exe" GOTO DEFAULTBUILDLOCALE
+setlocal enableextensions enabledelayedexpansion
+
+if not exist "%~dp0vswhere.exe" GOTO DEFAULTBUILDLOCALE
 
 REM VSWhere from https://github.com/Microsoft/vswhere use to locate highest installed VS Installation
 for /f "usebackq tokens=1* delims=: " %%i in (`vswhere.exe -latest -requires Microsoft.VisualStudio.Workload.NativeDesktop`) do (
@@ -16,7 +18,9 @@ IF "%VS2017TOOLS%" == "" GOTO DEFAULTBUILDLOCALE
 IF [%1] == [] cmd /C "%VSDir%\Common7\Tools\vsdevcmd.bat && cd ""%~dp0"" && msbuild release.xml /target:BuildLocale"
 IF NOT [%1] == [] cmd /C "%VSDir%\Common7\Tools\vsdevcmd.bat && cd ""%~dp0"" && msbuild release.xml /target:BuildAndCopyLocale /property:LocaleTargetDir=%1"
 
+GOTO END
 :DEFAULTBUILDLOCALE
+
 IF [%1] == [] cmd /C "vsdevcmd.bat && cd ""%~dp0"" && msbuild release.xml /target:BuildLocale"
 IF NOT [%1] == [] cmd /C "vsdevcmd.bat && cd ""%~dp0"" && msbuild release.xml /target:BuildAndCopyLocale /property:LocaleTargetDir=%1"
 :END
