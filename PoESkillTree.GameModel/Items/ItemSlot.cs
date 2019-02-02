7ae8b7bc0d21f3a92188d0ed749fa738d8dda875
brather1ng <7596346+brather1ng@users.noreply.github.com>
2019-02-02 21:56:44 +0100

    Add flasks to the inventory

--- PoESkillTree.GameModel/Items/ItemSlot.cs
+++ PoESkillTree.GameModel/Items/ItemSlot.cs
@@ -8,101 +8,22 @@ namespace PoESkillTree.GameModel.Items
     [Flags]
     public enum ItemSlot: uint//Requires 2147483648 worth of space to fit all jewel sockets as flags (and 64 bit+ if want any extra jewel slots etc)
     {
-        Unequipable = 0x0,
-        BodyArmour = 0x1,
-        MainHand = 0x2,
-        OffHand = 0x4,
-        Ring = 0x8,
-        Ring2 = 0x10,
-        Amulet = 0x20,
-        Helm = 0x40,
-        Gloves = 0x80,
-        Boots = 0x100,
-        Gem = 0x200,
-        Belt = 0x400,
-        /// <summary>
-        /// (Int Threshold Jewel Slot)
-        /// </summary>
-        JSlot_Int_Witch = 0x800,
-        /// <summary>
-        /// (Int Threshold Jewel Slot)
-        /// </summary>
-        JSlot_Int_Scion = 0x1000,
-        /// <summary>
-        /// (Int Threshold Jewel Slot)
-        /// </summary>
-        JSlot_Int_WitchShadow = 0x2000,
-        /// <summary>
-        /// (Both Int and Dex Threshold Jewel Slot)
-        /// </summary>
-        JSlot_DexInt_Scion = 0x4000,
-        /// <summary>
-        /// (Both Str and Int Threshold Jewel Slot)
-        /// </summary>
-        JSlot_StrInt_Scion = 0x8000,
-        /// <summary>
-        /// (Both Str and Dex Threshold Jewel Slot)
-        /// </summary>
-        JSlot_StrDex_Scion = 0x10000,
-        /// <summary>
-        /// (Neutral Jewel Slot)
-        /// </summary>
-        JSlot_Neutral_Acrobatics = 0x20000,
-        /// <summary>
-        /// (Dex Threshold Jewel Slot)
-        /// </summary>
-        JSlot_Dex_ShadowRanger = 0x40000,
-        /// <summary>
-        /// (Both Int and Dex Threshold Jewel Slot)
-        /// </summary>
-        JSlot_DexInt_Shadow = 0x80000,
-        /// <summary>
-        /// (Dex Threshold Jewel)
-        /// </summary>
-        JSlot_Dex_Ranger = 0x100000,
-        /// <summary>
-        /// (Dex Threshold Jewel Slot)
-        /// </summary>
-        JSlot_Dex_RangerDuelist = 0x200000,
-        /// <summary>
-        /// (Str Threshold Jewel Slot)
-        /// </summary>
-        JSlot_Str_WarriorDuelist = 0x400000,
-        /// <summary>
-        /// (Str Threshold Jewel Slot)
-        /// </summary>
-        JSlot_Str_WarriorTemplarScion = 0x800000,
-        /// <summary>
-        /// (Int Threshold Jewel Slot)
-        /// </summary>
-        JSlot_Int_TemplarWitch = 0x1000000,
-        /// <summary>
-        /// (Str Threshold Jewel Slot)
-        /// </summary>
-        JSlot_Str_FarWarTempScion = 0x2000000,
-        /// <summary>
-        /// (Both Int and Str Threshold Jewel Slot)
-        /// </summary>
-        JSlot_StrInt_Templar = 0x4000000,
-        /// <summary>
-        /// (Both Str and Dex Threshold Jewel Slot)
-        /// </summary>
-        JSlot_StrDex_Duelist = 0x8000000,
-        /// <summary>
-        /// (Neutral Jewel Slot)
-        /// </summary>
-        JSlot_Neutral_IronGrip = 0x10000000,
-        /// <summary>
-        /// (Neutral Jewel Slot)
-        /// </summary>
-        JSlot_Neutral_PointBlank = 0x20000000,
-        /// <summary>
-        /// (Neutral Jewel Slot)
-        /// </summary>
-        JSlot_Neutral_MinionInstability = 0x40000000,
-        /// <summary>
-        /// (Str Threshold Jewel Slot)
-        /// </summary>
-        JSlot_Str_Warrior = 0x80000000
+        Unequipable = 0,
+        BodyArmour = 1 << 0,
+        MainHand = 1 << 1,
+        OffHand = 1 << 2,
+        Ring = 1 << 3,
+        Ring2 = 1 << 4,
+        Amulet = 1 << 5,
+        Helm = 1 << 6,
+        Gloves = 1 << 7,
+        Boots = 1 << 8,
+        Gem = 1 << 9,
+        Belt = 1 << 10,
+        Flask1 = 1 << 11,
+        Flask2 = 1 << 12,
+        Flask3 = 1 << 13,
+        Flask4 = 1 << 14,
+        Flask5 = 1 << 15,
     }
 }
--- WPFSKillTree/ViewModels/Equipment/InventoryViewModel.cs
+++ WPFSKillTree/ViewModels/Equipment/InventoryViewModel.cs
@@ -1,4 +1,5 @@
-﻿using PoESkillTree.GameModel.Items;
+﻿using System.Collections.Generic;
+using PoESkillTree.GameModel.Items;
 using POESKillTree.Model.Items;
 using POESKillTree.Utils;
 
@@ -22,6 +23,7 @@ namespace POESKillTree.ViewModels.Equipment
         public InventoryItemViewModel Gloves { get; }
         public InventoryItemViewModel Boots { get; }
         public InventoryItemViewModel Belt { get; }
+        public IReadOnlyList<InventoryItemViewModel> Flasks { get; }
 
         public InventoryViewModel(IExtendedDialogCoordinator dialogCoordinator, ItemAttributes itemAttributes)
         {
@@ -37,162 +39,46 @@ namespace POESKillTree.ViewModels.Equipment
             Gloves = CreateSlotVm(ItemSlot.Gloves);
             Boots = CreateSlotVm(ItemSlot.Boots);
             Belt = CreateSlotVm(ItemSlot.Belt);
-
-            JSlot_Int_Witch = CreateSlotVm(ItemSlot.JSlot_Int_Witch);
-            JSlot_Int_Scion = CreateSlotVm(ItemSlot.JSlot_Int_Scion);
-            JSlot_Int_WitchShadow = CreateSlotVm(ItemSlot.JSlot_Int_WitchShadow);
-            JSlot_DexInt_Scion = CreateSlotVm(ItemSlot.JSlot_DexInt_Scion);
-            JSlot_StrInt_Scion = CreateSlotVm(ItemSlot.JSlot_StrInt_Scion);
-            JSlot_StrDex_Scion = CreateSlotVm(ItemSlot.JSlot_StrDex_Scion);
-            JSlot_Neutral_Acrobatics = CreateSlotVm(ItemSlot.JSlot_Neutral_Acrobatics);
-            JSlot_Dex_ShadowRanger = CreateSlotVm(ItemSlot.JSlot_Dex_ShadowRanger);
-            JSlot_DexInt_Shadow = CreateSlotVm(ItemSlot.JSlot_DexInt_Shadow);
-            JSlot_Dex_Ranger = CreateSlotVm(ItemSlot.JSlot_Dex_Ranger);
-            JSlot_Dex_RangerDuelist = CreateSlotVm(ItemSlot.JSlot_Dex_RangerDuelist);
-            JSlot_Str_WarriorDuelist = CreateSlotVm(ItemSlot.JSlot_Str_WarriorDuelist);
-            JSlot_Str_WarriorTemplarScion = CreateSlotVm(ItemSlot.JSlot_Str_WarriorTemplarScion);
-            JSlot_Int_TemplarWitch = CreateSlotVm(ItemSlot.JSlot_Int_TemplarWitch);
-            JSlot_Str_FarWarTempScion = CreateSlotVm(ItemSlot.JSlot_Str_FarWarTempScion);
-            JSlot_StrInt_Templar = CreateSlotVm(ItemSlot.JSlot_StrInt_Templar);
-            JSlot_StrDex_Duelist = CreateSlotVm(ItemSlot.JSlot_StrDex_Duelist);
-            JSlot_Neutral_IronGrip = CreateSlotVm(ItemSlot.JSlot_Neutral_IronGrip);
-            JSlot_Neutral_PointBlank = CreateSlotVm(ItemSlot.JSlot_Neutral_PointBlank);
-            JSlot_Neutral_MinionInstability = CreateSlotVm(ItemSlot.JSlot_Neutral_MinionInstability);
-            JSlot_Str_Warrior = CreateSlotVm(ItemSlot.JSlot_Str_Warrior);
+            Flasks = new[]
+            {
+                CreateSlotVm(ItemSlot.Flask1),
+                CreateSlotVm(ItemSlot.Flask2),
+                CreateSlotVm(ItemSlot.Flask3),
+                CreateSlotVm(ItemSlot.Flask4),
+                CreateSlotVm(ItemSlot.Flask5),
+            };
         }
 
         private InventoryItemViewModel CreateSlotVm(ItemSlot slot)
         {
-            var imageName = slot.ToString();
-            if (slot == ItemSlot.MainHand)
-            {
-                imageName = "TwoHandSword";
-            }
-            else if (slot == ItemSlot.OffHand)
-            {
-                imageName = "Shield";
-            }
-            else if (slot == ItemSlot.Ring2)
-            {
-                imageName = "Ring";
-            }
-            else if (slot == ItemSlot.Helm)
-            {
-                imageName = "Helmet";
-            }
-            //(replace with dynamic jewel slots later)
-            else if(slot == ItemSlot.JSlot_Int_Witch || slot == ItemSlot.JSlot_Int_Scion || slot == ItemSlot.JSlot_Int_WitchShadow || slot == ItemSlot.JSlot_DexInt_Scion || slot == ItemSlot.JSlot_StrInt_Scion
-            || slot == ItemSlot.JSlot_StrDex_Scion || slot == ItemSlot.JSlot_Neutral_Acrobatics || slot == ItemSlot.JSlot_Dex_ShadowRanger || slot == ItemSlot.JSlot_DexInt_Shadow || slot == ItemSlot.JSlot_Dex_Ranger
-            || slot == ItemSlot.JSlot_Dex_RangerDuelist || slot == ItemSlot.JSlot_Str_WarriorDuelist || slot == ItemSlot.JSlot_Str_WarriorTemplarScion || slot == ItemSlot.JSlot_Int_TemplarWitch || slot == ItemSlot.JSlot_Str_FarWarTempScion
-            || slot == ItemSlot.JSlot_StrInt_Templar || slot == ItemSlot.JSlot_StrDex_Duelist || slot == ItemSlot.JSlot_Neutral_IronGrip || slot == ItemSlot.JSlot_Neutral_PointBlank || slot == ItemSlot.JSlot_Neutral_MinionInstability
-            || slot == ItemSlot.JSlot_Str_Warrior)
-            {
-                imageName = "Jewel";
-            }
-
+            var imageName = SlotToImageName(slot);
             return new InventoryItemViewModel(_dialogCoordinator, _itemAttributes, slot)
             {
                 EmptyBackgroundImagePath = $"/POESKillTree;component/Images/EquipmentUI/ItemDefaults/{imageName}.png"
             };
         }
 
-        /// <summary>
-        /// Calculates the total single attributes.(Mostly for SkillTree Generation Totals)
-        /// </summary>
-        /// <returns></returns>
-        public Dictionary<string, float> CalculateTotalSingleAttributes()
+        private static string SlotToImageName(ItemSlot slot)
         {
-            Dictionary<string, float> ItemDictionary = new Dictionary<string, float>();
-            POESKillTree.Model.Items.Item ItemData;
-            for (int Index = 0; Index < 31; ++Index)
+            switch (slot)
             {
-                switch (Index)
-                {
-                    case 0:
-                        ItemData = JSlot_DexInt_Scion.Item; break;
-                    case 1:
-                        ItemData = JSlot_DexInt_Shadow.Item; break;
-                    case 2:
-                        ItemData = JSlot_Dex_RangerDuelist.Item; break;
-                    case 3:
-                        ItemData = JSlot_Dex_Ranger.Item; break;
-                    case 4:
-                        ItemData = JSlot_Dex_ShadowRanger.Item; break;
-                    case 5:
-                        ItemData = JSlot_Int_Scion.Item; break;
-                    case 6:
-                        ItemData = JSlot_Int_TemplarWitch.Item; break;
-                    case 7:
-                        ItemData = JSlot_Int_Witch.Item; break;
-                    case 8:
-                        ItemData = JSlot_Int_WitchShadow.Item; break;
-                    case 9:
-                        ItemData = JSlot_StrDex_Duelist.Item; break;
-                    case 10:
-                        ItemData = JSlot_StrDex_Scion.Item; break;
-                    case 11:
-                        ItemData = JSlot_StrInt_Scion.Item; break;
-                    case 12:
-                        ItemData = JSlot_StrInt_Templar.Item; break;
-                    case 13:
-                        ItemData = JSlot_Str_FarWarTempScion.Item; break;
-                    case 14:
-                        ItemData = JSlot_Str_WarriorDuelist.Item; break;
-                    case 15:
-                        ItemData = JSlot_Str_Warrior.Item; break;
-                    case 16:
-                        ItemData = JSlot_Str_WarriorTemplarScion.Item; break;
-                    case 17:
-                        ItemData = JSlot_Neutral_Acrobatics.Item; break;
-                    case 18:
-                        ItemData = JSlot_Neutral_IronGrip.Item; break;
-                    case 19:
-                        ItemData = JSlot_Neutral_MinionInstability.Item; break;
-                    case 20:
-                        ItemData = JSlot_Neutral_PointBlank.Item; break;
-                    //Non-Jewel Items Below
-                    case 21:
-                        ItemData = Armor.Item; break;
-                    case 22:
-                        ItemData = MainHand.Item; break;
-                    case 23:
-                        ItemData = OffHand.Item; break;
-                    case 24:
-                        ItemData = Ring.Item; break;
-                    case 25:
-                        ItemData = Ring2.Item; break;
-                    case 26:
-                        ItemData = Amulet.Item; break;
-                    case 27:
-                        ItemData = Helm.Item; break;
-                    case 28:
-                        ItemData = Gloves.Item; break;
-                    case 29:
-                        ItemData = Boots.Item; break;
-                    case 30:
-                        ItemData = Belt.Item; break;
-                    default://Should never use default
-                        ItemData = null; break;
-                }
-                if (ItemData != null)
-                {
-                    foreach (var TargetMod in ItemData.Mods)
-                    {
-                        if (TargetMod.Values.Count == 1)//Only single value Mods added to dictionary for solver use
-                        {
-                            if(ItemDictionary.ContainsKey(TargetMod.Attribute))
-                            {
-                                ItemDictionary[TargetMod.Attribute] += TargetMod.Values[0];
-                            }
-                            else
-                            {
-                                ItemDictionary.Add(TargetMod.Attribute, TargetMod.Values[0]);
-                            }
-                        }
-                    }
-                }
+                case ItemSlot.MainHand:
+                    return "TwoHandSword";
+                case ItemSlot.OffHand:
+                    return "Shield";
+                case ItemSlot.Ring2:
+                    return "Ring";
+                case ItemSlot.Helm:
+                    return "Helmet";
+                case ItemSlot.Flask1:
+                case ItemSlot.Flask2:
+                case ItemSlot.Flask3:
+                case ItemSlot.Flask4:
+                case ItemSlot.Flask5:
+                    return "LifeFlask";
+                default:
+                    return slot.ToString();
             }
-            return ItemDictionary;
         }
     }
 }
