fbf0a8e7c062d10707cb171f556525087fafbfeb
BlazesRus <BlazesRus@yahoo.com>
2019-02-09 16:19:17 -0500

    Adjusted CalculateTotalSingleAttributes to use dynamic jewel dictionary and added more parts yo JewelNodeData

--- WPFSKillTree/ViewModels/Equipment/InventoryViewModel.cs
+++ WPFSKillTree/ViewModels/Equipment/InventoryViewModel.cs
@@ -26,7 +26,30 @@ namespace POESKillTree.ViewModels.Equipment
         public InventoryItemViewModel Belt { get; }
         public IReadOnlyList<InventoryItemViewModel> Flasks { get; }
 
-        public InventoryViewModel(IExtendedDialogCoordinator dialogCoordinator, ItemAttributes itemAttributes)
+  //      public InventoryItemViewModel JSlot_Int_Witch { get; }
+  //      public InventoryItemViewModel JSlot_Int_Scion { get; }
+  //      public InventoryItemViewModel JSlot_Int_WitchShadow { get; }
+  //      public InventoryItemViewModel JSlot_DexInt_Scion { get; }
+  //      public InventoryItemViewModel JSlot_StrInt_Scion { get; }
+  //      public InventoryItemViewModel JSlot_StrDex_Scion { get; }
+  //      public InventoryItemViewModel JSlot_Neutral_Acrobatics { get; }
+  //      public InventoryItemViewModel JSlot_Dex_ShadowRanger { get; }
+  //      public InventoryItemViewModel JSlot_DexInt_Shadow { get; }
+  //      public InventoryItemViewModel JSlot_Dex_Ranger { get; }
+  //      public InventoryItemViewModel JSlot_Dex_RangerDuelist { get; }
+  //      public InventoryItemViewModel JSlot_Str_WarriorDuelist { get; }
+  //      public InventoryItemViewModel JSlot_Str_WarriorTemplarScion { get; }
+  //      public InventoryItemViewModel JSlot_Int_TemplarWitch { get; }
+  //      public InventoryItemViewModel JSlot_Str_FarWarTempScion { get; }
+  //      public InventoryItemViewModel JSlot_StrInt_Templar { get; }
+  //      public InventoryItemViewModel JSlot_StrDex_Duelist { get; }
+  //      public InventoryItemViewModel JSlot_Neutral_IronGrip { get; }
+  //      public InventoryItemViewModel JSlot_Neutral_PointBlank { get; }
+  //      public InventoryItemViewModel JSlot_Neutral_MinionInstability { get; }
+  //      public InventoryItemViewModel JSlot_Str_Warrior { get; }
+
+        public InventoryViewModel(IExtendedDialogCoordinator dialogCoordinator, EquipmentData equipmentData,
+            ItemAttributes itemAttributes)
         {
             _dialogCoordinator = dialogCoordinator;
             _itemAttributes = itemAttributes;
@@ -40,7 +63,28 @@ namespace POESKillTree.ViewModels.Equipment
             Gloves = CreateSlotVm(ItemSlot.Gloves);
             Boots = CreateSlotVm(ItemSlot.Boots);
             Belt = CreateSlotVm(ItemSlot.Belt);
-            Flasks = ItemSlotExtensions.Flasks.Select(CreateSlotVm).ToList();
+
+   //         JSlot_Int_Witch = CreateSlotVm(ItemSlot.JSlot_Int_Witch);
+   //         JSlot_Int_Scion = CreateSlotVm(ItemSlot.JSlot_Int_Scion);
+   //         JSlot_Int_WitchShadow = CreateSlotVm(ItemSlot.JSlot_Int_WitchShadow);
+   //         JSlot_DexInt_Scion = CreateSlotVm(ItemSlot.JSlot_DexInt_Scion);
+   //         JSlot_StrInt_Scion = CreateSlotVm(ItemSlot.JSlot_StrInt_Scion);
+   //         JSlot_StrDex_Scion = CreateSlotVm(ItemSlot.JSlot_StrDex_Scion);
+   //         JSlot_Neutral_Acrobatics = CreateSlotVm(ItemSlot.JSlot_Neutral_Acrobatics);
+   //         JSlot_Dex_ShadowRanger = CreateSlotVm(ItemSlot.JSlot_Dex_ShadowRanger);
+   //         JSlot_DexInt_Shadow = CreateSlotVm(ItemSlot.JSlot_DexInt_Shadow);
+   //         JSlot_Dex_Ranger = CreateSlotVm(ItemSlot.JSlot_Dex_Ranger);
+   //         JSlot_Dex_RangerDuelist = CreateSlotVm(ItemSlot.JSlot_Dex_RangerDuelist);
+   //         JSlot_Str_WarriorDuelist = CreateSlotVm(ItemSlot.JSlot_Str_WarriorDuelist);
+   //         JSlot_Str_WarriorTemplarScion = CreateSlotVm(ItemSlot.JSlot_Str_WarriorTemplarScion);
+   //         JSlot_Int_TemplarWitch = CreateSlotVm(ItemSlot.JSlot_Int_TemplarWitch);
+   //         JSlot_Str_FarWarTempScion = CreateSlotVm(ItemSlot.JSlot_Str_FarWarTempScion);
+   //         JSlot_StrInt_Templar = CreateSlotVm(ItemSlot.JSlot_StrInt_Templar);
+   //         JSlot_StrDex_Duelist = CreateSlotVm(ItemSlot.JSlot_StrDex_Duelist);
+   //         JSlot_Neutral_IronGrip = CreateSlotVm(ItemSlot.JSlot_Neutral_IronGrip);
+   //         JSlot_Neutral_PointBlank = CreateSlotVm(ItemSlot.JSlot_Neutral_PointBlank);
+   //         JSlot_Neutral_MinionInstability = CreateSlotVm(ItemSlot.JSlot_Neutral_MinionInstability);
+   //         JSlot_Str_Warrior = CreateSlotVm(ItemSlot.JSlot_Str_Warrior);
         }
 
         private InventoryItemViewModel CreateSlotVm(ItemSlot slot)
@@ -48,26 +92,107 @@ namespace POESKillTree.ViewModels.Equipment
             var imageName = SlotToImageName(slot);
             return new InventoryItemViewModel(_dialogCoordinator, _itemAttributes, slot)
             {
+                imageName = "TwoHandSword";
+            }
+            else if (slot == ItemSlot.OffHand)
+            {
+                imageName = "Shield";
+            }
+            else if (slot == ItemSlot.Ring2)
+            {
+                imageName = "Ring";
+            }
+            else if (slot == ItemSlot.Helm)
+            {
+                imageName = "Helmet";
+            }
+   //         //(replace with dynamic jewel slots later)
+   //         else if(slot == ItemSlot.JSlot_Int_Witch || slot == ItemSlot.JSlot_Int_Scion || slot == ItemSlot.JSlot_Int_WitchShadow || slot == ItemSlot.JSlot_DexInt_Scion || slot == ItemSlot.JSlot_StrInt_Scion
+   //         || slot == ItemSlot.JSlot_StrDex_Scion || slot == ItemSlot.JSlot_Neutral_Acrobatics || slot == ItemSlot.JSlot_Dex_ShadowRanger || slot == ItemSlot.JSlot_DexInt_Shadow || slot == ItemSlot.JSlot_Dex_Ranger
+   //         || slot == ItemSlot.JSlot_Dex_RangerDuelist || slot == ItemSlot.JSlot_Str_WarriorDuelist || slot == ItemSlot.JSlot_Str_WarriorTemplarScion || slot == ItemSlot.JSlot_Int_TemplarWitch || slot == ItemSlot.JSlot_Str_FarWarTempScion
+   //         || slot == ItemSlot.JSlot_StrInt_Templar || slot == ItemSlot.JSlot_StrDex_Duelist || slot == ItemSlot.JSlot_Neutral_IronGrip || slot == ItemSlot.JSlot_Neutral_PointBlank || slot == ItemSlot.JSlot_Neutral_MinionInstability
+   //         || slot == ItemSlot.JSlot_Str_Warrior)
+   //         {
+   //             imageName = "Jewel";
+   //         }
+
+            return new InventoryItemViewModel(_dialogCoordinator, _equipmentData, _itemAttributes, slot)
+            {
                 EmptyBackgroundImagePath = $"/POESKillTree;component/Images/EquipmentUI/ItemDefaults/{imageName}.png"
             };
         }
 
         private static string SlotToImageName(ItemSlot slot)
         {
-            if (slot.IsFlask())
-                return "LifeFlask";
-            switch (slot)
+            Dictionary<string, float> ItemDictionary = new Dictionary<string, float>();
+            POESKillTree.Model.Items.Item ItemData;
+			int JewelIndex = 0;
+			bool ContinueCalc = true;
+            for (int Index = 0; ContinueCalc; ++Index)
             {
-                case ItemSlot.MainHand:
-                    return "TwoHandSword";
-                case ItemSlot.OffHand:
-                    return "Shield";
-                case ItemSlot.Ring2:
-                    return "Ring";
-                case ItemSlot.Helm:
-                    return "Helmet";
-                default:
-                    return slot.ToString();
+                switch (Index)
+                {
+                    case 0:
+                        ItemData = Armor.Item; break;
+                    case 1:
+                        ItemData = MainHand.Item; break;
+                    case 2:
+                        ItemData = OffHand.Item; break;
+                    case 3:
+                        ItemData = Ring.Item; break;
+                    case 4:
+                        ItemData = Ring2.Item; break;
+                    case 5:
+                        ItemData = Amulet.Item; break;
+                    case 6:
+                        ItemData = Helm.Item; break;
+                    case 7:
+                        ItemData = Gloves.Item; break;
+                    case 8:
+                        ItemData = Boots.Item; break;
+                    case 9:
+                        ItemData = Belt.Item; break;
+                    default:
+						foreach (var JewelSlotData in GlobalSettings.JewelInfo)
+						{
+							ItemData = JewelSlotData.JewelData;
+							if (ItemData != null)
+							{
+								foreach (var TargetMod in ItemData.Mods)
+								{
+									if (TargetMod.Values.Count == 1)//Only single value Mods added to dictionary for solver use
+									{
+										if(ItemDictionary.ContainsKey(TargetMod.Attribute))
+										{
+											ItemDictionary[TargetMod.Attribute] += TargetMod.Values[0];
+										}
+										else
+										{
+											ItemDictionary.Add(TargetMod.Attribute, TargetMod.Values[0]);
+										}
+									}
+								}
+							}
+						}
+						ItemData = null; break;
+                }
+                if (ItemData != null)
+                {
+                    foreach (var TargetMod in ItemData.Mods)
+                    {
+                        if (TargetMod.Values.Count == 1)//Only single value Mods added to dictionary for solver use
+                        {
+                            if(ItemDictionary.ContainsKey(TargetMod.Attribute))
+                            {
+                                ItemDictionary[TargetMod.Attribute] += TargetMod.Values[0];
+                            }
+                            else
+                            {
+                                ItemDictionary.Add(TargetMod.Attribute, TargetMod.Values[0]);
+                            }
+                        }
+                    }
+                }
             }
         }
     }
