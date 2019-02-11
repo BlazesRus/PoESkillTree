57e887ef77214ce4bb2bc8d842ddb30630287dfd
BlazesRus <BlazesRus@yahoo.com>
2019-02-11 13:17:04 -0500

    Added Notifier code into JewelData

--- WPFSKillTree/SkillTreeFiles/JewelAndTrackedData.cs
+++ WPFSKillTree/SkillTreeFiles/JewelAndTrackedData.cs
@@ -17,6 +17,8 @@ using POESKillTree.Model.Items;
 using POESKillTree.ViewModels.Equipment;
 using POESKillTree.SkillTreeFiles;
 
+using System.Runtime.CompilerServices;//Needed for Notifier parts of JewelData
+
 namespace POESKillTree
 {
     /// <summary>
@@ -51,7 +53,68 @@ namespace POESKillTree
     /// </summary>
     /// <seealso cref="System.Collections.Generic.Dictionary{System.UInt16, POESKillTree.JewelNodeData}" />
     public class JewelData : Dictionary<ushort, JewelNodeData>
-    {
+    {//Notifier combined directly into class since can't have 2 base classes
+        #region NotifierCode
+        /// <summary>
+        /// Sets <paramref name="backingStore"/> to <paramref name="value"/> and
+        /// raises <see cref="PropertyChanging"/> before and <see cref="PropertyChanged"/>
+        /// after setting the value.
+        /// </summary>
+        /// <param name="backingStore">Target variable</param>
+        /// <param name="value">Source variable</param>
+        /// <param name="onChanged">Called after changing the value but before raising <see cref="PropertyChanged"/>.</param>
+        /// <param name="onChanging">Called before changing the value and before raising <see cref="PropertyChanging"/> with <paramref name="value"/> as parameter.</param>
+        /// <param name="propertyName">Name of the changed property</param>
+        protected void SetProperty<T>(
+            ref T backingStore, T value,
+            Action onChanged = null,
+            Action<T> onChanging = null,
+            [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value)) return;
+
+            onChanging?.Invoke(value);
+            OnPropertyChanging(propertyName);
+
+            backingStore = value;
+
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+        }
+
+        /// <summary>
+        /// INotifyPropertyChanged event that is called right before a property is changed.
+        /// </summary>
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        private void OnPropertyChanging(string propertyName)
+        {
+            PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// INotifyPropertyChanged event that is called right after a property is changed.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Equivalent to <c>o.MemberwiseClone()</c> except that events are set to null.
+        /// Override if your subclass has events or if you need to re-register handlers.
+        /// </summary>
+        protected virtual JewelData SafeMemberwiseClone()
+        {
+            var t = (JewelData)MemberwiseClone();
+            t.PropertyChanged = null;
+            t.PropertyChanging = null;
+            return t;
+        }
+#endregion
+
         /// <summary>
         /// Keys for Strength Threshold Jewel Slots
         /// </summary>
