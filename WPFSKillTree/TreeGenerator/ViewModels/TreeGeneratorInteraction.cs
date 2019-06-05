9d29e0c345b9c6f66ead2823957967db3111e2ce
James Michael Armstrong(BlazesRus) <BlazesRus@yahoo.com>
2019-06-04 20:40:17 -0400

    Rest of the conflicting files merged together

--- WPFSKillTree/TreeGenerator/ViewModels/TreeGeneratorInteraction.cs
+++ WPFSKillTree/TreeGenerator/ViewModels/TreeGeneratorInteraction.cs
@@ -53,12 +53,6 @@ namespace PoESkillTree.TreeGenerator.ViewModels
             }
         }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="TreeGeneratorInteraction"/> class.
-        /// </summary>
-        /// <param name="dialogCoordinator">The dialog coordinator.</param>
-        /// <param name="persistentData">The persistent data.</param>
-        /// <param name="skillTree">The skill tree.</param>
         public TreeGeneratorInteraction(ISettingsDialogCoordinator dialogCoordinator, IPersistentData persistentData,
             SkillTree skillTree)
         {
